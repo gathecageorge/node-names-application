1a65a9cb6a065badeb4ca711d718674c2bfa4c9b
George Gatheca <gatheca.george@gmail.com>
2024-08-22 16:16:37 +0100

    change container name && workspace folder

--- .devcontainer/fix-ownership.sh
+++ .devcontainer/fix-ownership.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-if [ $# -ge 1 ]; then
-  folder_path="$1"
-else
-  folder_path="/workspaces"
-fi
-
-sudo chown USERNAME:USERNAME $folder_path
+sudo chown USERNAME:USERNAME /node-names-application
